c73087e5c5ce55f81a96e6826c0ed620fe641a16
charlie.ashton <19330235+charlieashton@users.noreply.github.com>
2021-07-15 11:52:59 +0100

    updated dependancies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "webdriver-factory" % "0.15.0"  % Test,
+    "uk.gov.hmrc"         %% "webdriver-factory" % "0.22.0"  % Test,
     "org.scalatest"       %% "scalatest"         % "3.2.0"   % Test,
     "org.scalatestplus"   %% "selenium-3-141"    % "3.2.0.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"      % "0.35.10" % Test,
@@ -11,7 +11,7 @@ object Dependencies {
     "io.cucumber"          % "cucumber-junit"    % "6.1.1"   % Test,
     "junit"                % "junit"             % "4.12"    % Test,
     "com.novocode"         % "junit-interface"   % "0.11"    % Test,
-    "uk.gov.hmrc"         %% "zap-automation"    % "2.8.0"   % Test,
+    "uk.gov.hmrc"         %% "zap-automation"    % "2.10.0"   % Test,
     "com.typesafe.play"   %% "play-ws"           % "2.6.20"  % Test,
     "com.typesafe"         % "config"            % "1.3.2"   % Test
   )
